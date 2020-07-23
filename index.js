b1d89a5626df0e1e95a7f4b8a4da5f3c449b56ad
kossanr <58185702+kossanr@users.noreply.github.com>
2020-07-23 11:54:27 -0600

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const header = {};
-
 function getRepo(userHandle) {
   fetch(`https://api.github.com/users/${userHandle}/repos`)
     .then((res) => res.json()) //format response as JSON
     .then((responseJson) => console.log(responseJson)); //log response to console
-
-  function watchForm() {
-    $("form").submit((e) => {
-      event.preventDefault();
-      const userHandle = $(".user-handle").val(); //captures value of user's input
-      getRepo(userHandle);
-    });
-  }
 }
-$(getRepo); //returns JSON object
+function watchForm() {
+  $("form").submit((e) => {
+    e.preventDefault();
+    const userHandle = $(".user-handle").val(); //captures value of user's input
+    getRepo(userHandle);
+  });
+}
+$(watchForm); //returns JSON object
